528833213ff7fec5d0c31dd4b682e18c9089a17b
Muhammadrasul Rejapov <muhammadrasulrejapov@email.com>
2025-03-31 20:26:38 +0500

    Qator oxirlari muammosi tuzatildi

--- EventBookingApi/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-using EventBookingApi.Data; 
-using EventBookingApi.Models;
-
-namespace EventBookingApi.Models
-{
-    public class ApplicationUser : IdentityUser { }
-}
